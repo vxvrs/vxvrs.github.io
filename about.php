ef92b1e3cfe1bda38502fccd3b015321fe547be7
vxvrs <v.overes@icloud.com>
2020-05-26 17:30:00 +0200

    Started working on about page

---
+++ about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About by Viggo Overes</title>
+        <link rel="stylesheet", href="style.css">
+    </head>
+
+    <body>
+        <?php include('head.php'); ?>
+
+        <div class="body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Aenean cursus sem sapien, sed imperdiet lacus gravida at. 
+                Nunc eget maximus leo. Vivamus porttitor sagittis lacus, tincidunt interdum justo blandit et. 
+                Nunc convallis odio enim, vitae mattis orci commodo vitae. 
+                Suspendisse quis magna mollis, placerat mauris non, lacinia est. 
+                Nullam semper viverra leo, vitae facilisis purus venenatis sed. 
+                Aliquam nec pharetra libero, ac blandit mauris. Duis euismod iaculis lectus at tempor. 
+                Mauris interdum nisl eget dui tincidunt, vitae fringilla orci sagittis. 
+                Nulla imperdiet nunc nec sapien convallis, sed varius justo aliquam. 
+                Cras tincidunt iaculis commodo. 
+                Pellentesque pharetra, magna interdum consequat lacinia, nunc risus lobortis turpis, at venenatis mauris nisl non nunc.
+                Donec et varius metus, vitae suscipit elit. 
+                Sed ac ornare mauris, quis porttitor sapien. Proin eu venenatis ipsum. 
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                Nam semper mauris in sollicitudin volutpat. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+                Sed hendrerit velit sed lorem vulputate, eu lobortis magna condimentum. 
+                Integer sit amet mattis metus, eget imperdiet mauris. Nam ut cursus mauris. 
+                In hac habitasse platea dictumst. Nulla hendrerit vehicula magna, at fringilla leo facilisis ut. 
+                Vestibulum quis suscipit neque. Nulla.</p>
+        </div>
+    </body>
+</html>

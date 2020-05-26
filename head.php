eee2bca97fe5e5ffb95dde08b894f6bb580c5b47
vxvrs <v.overes@icloud.com>
2020-05-26 17:35:06 +0200

    Removed php

--- head.php
+++
@@ -1,8 +0,0 @@
-<div class="head">
-    <h1 class="title">Website by Viggo Overes</h1>
-
-    <nav>
-        <a class="link" href="index.php">home</a>
-        <a class="link" href="about.php">about</a>
-    </nav>
-</div>
